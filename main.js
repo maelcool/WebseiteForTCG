e210d5c14e31490e8118d0b90d208b78e57982d4
Mael Pittet <maelpittet@gmail.com>
2026-03-03 13:30:38 +0100

    Fixed Display

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQZx3ik0FpEurNSQieDO0e69d2dhglDVL0wg0iBdmErqTXJ7fARSmLtPhDs2KGWBg8NBCbvAIdanvZy/pub?output=csv"
 const cardDisplay = document.getElementById("CardDisplay");
 const searchName = document.getElementById("searchName");
+const sheetCode = "1p8Q9kmKCNCVNvKaVdelb9MtS1WdwdtSGKbFHr5wvOgU"
 
 let cardsData = []; 
 
@@ -12,6 +13,7 @@ async function loadCards() {
         // Parse CSV using PapaParse
         const result = Papa.parse(csvText, { header: true });
         cardsData = result.data;
+        displayCards(cardsData);
         
     } catch (err) {
         cardDisplay.innerHTML = "Error loading cards: " + err;
@@ -24,25 +26,42 @@ function displayCards(cards) {
         cardDisplay.innerHTML = "<p>No cards found.</p>";
         return;
     }
+    let html = "<div class=\"card-container\">";
 
-    cardDisplay.innerHTML = cards.map(card => `
-        <div class="card">
-            <h3>${card.Name}</h3>
-            <p>Type: ${card.Type}</p>
-            <p>Attack: ${card.Attack}</p>
-            <p>Defense: ${card.Defense}</p>
-        </div>
-    `).join('');
+    for (const card of cards) {
+        if (!card.KarteGanz){
+            html += `
+            <div class="card">
+                <h3>${card.Name}</h3>
+                <p>Bild nicht verfügbar</p>
+            </div>
+            `;
+        }
+        else{
+            const imageId = card.KarteGanz.split("/d/")[1].split("/")[0];
+            html += `
+                <div class="card">
+                    <h3>${card.Name}</h3>
+                    <img src="https://drive.google.com/thumbnail?id=${imageId}" alt="Bild von ${card.Name}" class="card-image">
+                </div>
+            `;
+        }
+    }
+    html += "</div>";
+    cardDisplay.innerHTML = html;
 }
 
 searchName.addEventListener("input", () => {
     const query = searchName.value.toLowerCase();
-
+    if (!query) {
+        displayCards(cardsData);
+        return;
+    }
     const filtered = cardsData.filter(card =>
-        Object.values(card).some(value =>
-            value.toLowerCase().includes(query)
-        )
+        card.Name.toLowerCase().includes(query)
     );
 
     displayCards(filtered);
 });
+
+loadCards()
